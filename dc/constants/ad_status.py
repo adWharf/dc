6ad4dab0ecd18685db21f36aa9d7ac26142af610
IamBusy <1342247033@qq.com>
2018-05-02 15:45:14 +0800

    Add ad status constants

---
+++ dc/constants/ad_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@author: william
+@contact: 1342247033@qq.com
+@site: http://www.xiaolewei.com
+@file: ad_status.py
+@time: 12/04/2018 17:10
+"""
+ADSTATUS_UNKNOW  = -1    # 未知
+ADSTATUS_NORMAL  = 0     # 正常 
+ADSTATUS_PENDING = 1     # 待审核 
+ADSTATUS_DENIED  = 2     # 审核不通过 
+ADSTATUS_FROZEN  = 3     # 冻结 
+ADSTATUS_SUSPEND = 4     # 挂起 
+ADSTATUS_PREPARE = 5     # 准备状态 
+ADSTATUS_DELETED = 6     # 删除

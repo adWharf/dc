7a2bcc7fc3f63db45c193e5df9af1f2e2c7153fb
IamBusy <1342247033@qq.com>
2018-05-11 16:00:12 +0800

    Schedule: improve

---
+++ dc/schedule.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@author: william
+@contact: 1342247033@qq.com
+@site: http://www.xiaolewei.com
+@file: schedule.py
+@time: 11/05/2018 14:42
+"""
+from dc.schedule.scheduler import Scheduler
+
+if __name__ == '__main__':
+    schedule = Scheduler()
+    schedule.run()

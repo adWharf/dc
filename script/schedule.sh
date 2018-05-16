8920b7181ec03f8344f7d348182086eec15a5fdb
IamBusy <1342247033@qq.com>
2018-05-16 15:22:09 +0800

    :fountain_pen: add schedule

---
+++ script/schedule.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+docker stop schedule;
+docker rm schedule;
+docker build -t schedule -f Dockerfile-schedule .;
+docker run --name schedule --network dc --network-alias schedule -d schedule;

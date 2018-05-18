8ae19440af35242d39cfeef3473dbe778fd58a20
IamBusy <1342247033@qq.com>
2018-05-18 16:56:56 +0800

    Migration: enable null

--- dc/migrations/strategy.py
+++ dc/migrations/strategy.py
@@ -83,8 +83,8 @@ def migrate(schema: Schema):
             table.big_integer('account_id')
             table.big_integer('campaign_id')
             table.text('action')  # 调整动作
-            table.text('value')   # 调整值
-            table.text('extra')   # 额外参数
+            table.text('value').nullable()   # 调整值
+            table.text('extra').nullable()   # 额外参数
             table.text('triggered_point').nullable()  # 触发该action的记录
             table.text('resp_cnt').nullable()         # 执行结果内容
             table.string('resp_status_code').nullable()  # 执行结果状态
